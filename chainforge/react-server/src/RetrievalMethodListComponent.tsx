884462be8ed6cb928ea11eeb2773eb0ae6b875de
Ian Arawjo <fatso784@gmail.com>
2025-11-16 19:33:31 -0500

    Fix dark mode retriever list

--- chainforge/react-server/src/RetrievalMethodListComponent.tsx
+++ chainforge/react-server/src/RetrievalMethodListComponent.tsx
@@ -475,7 +475,6 @@ const RetrievalMethodListItem: React.FC<
             : 6,
 
           // row look & single-line layout
-          background: "white",
           boxShadow: "0 1px 3px rgba(0,0,0,.12), 0 1px 2px rgba(0,0,0,.24)",
           padding: "6px 8px",
           display: "grid",
